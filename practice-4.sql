b92a2395f8fafd443bb3ee73b0f67986a6adcca4
gargbydesign <amit.garg@kellogg.northwestern.edu>
2026-01-19 02:55:49 +0000

    solved all practice and created schema

--- practice-4.sql
+++ practice-4.sql
@@ -5,3 +5,4 @@
 -- +----------+
 
 
+SELECT neighborhood, COUNT(*) FROM listings WHERE neighborhood = 'Lincoln Park';
--- practice-6.sql
+++ practice-6.sql
@@ -15,4 +15,10 @@
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
 
+SELECT listings.neighborhood, COUNT (*)
+FROM reviews 
+INNER JOIN listings ON listings.id = reviews.listing_id
+GROUP BY listings.neighborhood
+ORDER BY listings.neighborhood
+;
 
